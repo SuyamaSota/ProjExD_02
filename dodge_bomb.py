79c282db6c6d7028b1dac3b5a195cbd3e51499e8
sota <c0a2208976@edu.teu.ac.jp>
2023-11-28 16:21:44 +0900

    演習1　方向

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,13 +3,16 @@ import pygame as pg
 import random
 
 
+
+
 WIDTH, HEIGHT = 1600, 900
-delta = { 
-        pg.K_UP: (0,-50),
-        pg.K_DOWN: (0,+50),
-        pg.K_LEFT: (-100,0),
-        pg.K_RIGHT: (+100,0)
+delta = {
+        pg.K_UP: (0,-5),
+        pg.K_DOWN: (0,+5),
+        pg.K_LEFT: (-5,0),
+        pg.K_RIGHT: (+5,0)
         }
+
 
 def check_bound(obj_rct: pg.Rect):
     yoko,tate = True,True
@@ -19,49 +22,95 @@ def check_bound(obj_rct: pg.Rect):
         tate = False
     return yoko,tate
 
+
+def kk_k():
+    kk_img0 = pg.transform.rotozoom(pg.image.load("ex02/fig/3.png"), 0, 2.0)
+    kk_img1 = pg.transform.flip(kk_img0, True, False)
+    return{
+        (0, 0):kk_img0,
+        (-5, 0):kk_img0,
+        (-5, -5):pg.transform.rotozoom(kk_img0, -45, 1.0),
+        (-5, +5):pg.transform.rotozoom(kk_img0, 45, 1.0),
+        (0, -5):pg.transform.rotozoom(kk_img1, 90, 1.0),
+        (+5, -5):pg.transform.rotozoom(kk_img1, 45, 1.0),
+        (+5, 0):pg.transform.rotozoom(kk_img1, 0, 1.0),
+        (+5, +5):pg.transform.rotozoom(kk_img1, -45, 1.0),
+        (0, +5):pg.transform.rotozoom(kk_img1, -90, 1.0)
+        }
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_imgs = kk_k()
+    kk_img = kk_imgs[(0,0)]
+
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900,400
 
+
     bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10) 
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0,WIDTH)
     bb_rct.centery = random.randint(0,HEIGHT)
-    vx, vy = +50, +50
+    vx, vy = +10, +10
 
-
+    saccs = [a for a in range(1, 11)]
 
 
 
     clock = pg.time.Clock()
     tmr = 0
+
+
+    accs = [a for a in range(1,11)]
+    if tmr == 10*accs[1]:
+        vx, vy = +10, +10
+
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-        
+       
         if kk_rct.colliderect(bb_rct):
             print("Game Over")
             return
-            
+           
         key_lst =pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_img = kk_imgs[(0,-5)]
+        if key_lst[pg.K_DOWN]:
+            kk_img = kk_imgs[(0,+5)]
+        if key_lst[pg.K_LEFT]:
+            kk_img = kk_imgs[(-5,0)]
+            if key_lst[pg.K_UP]:
+                kk_img = kk_imgs[(-5,-5)]
+            if key_lst[pg.K_DOWN]:
+                kk_img = kk_imgs[(-5,+5)]
+        if key_lst[pg.K_RIGHT]:
+            kk_img = kk_imgs[(+5,0)]
+            if key_lst[pg.K_UP]:
+                kk_img = kk_imgs[(+5,-5)]
+            if key_lst[pg.K_DOWN]:
+                kk_img = kk_imgs[(+5,+5)]
+
         sum_mv=[0,0]
         for k, tpl in delta.items():
             if key_lst[k]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
 
+
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0],sum_mv[1])
         if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img,bb_rct)
         bb_rct.move_ip(vx, vy)
@@ -75,8 +124,11 @@ def main():
         clock.tick(50)
 
 
+
+
 if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
     sys.exit()
+
